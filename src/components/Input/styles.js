b2ce6504f78c2915e7f3c2d62787b347e39477e0
RaphaelMolina <manson1307@gmail.com>
2022-12-03 20:08:54 -0300

    Projeto Calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 75px;
+    background-color: #AAAAFF;
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 75px;
+        background-color: #AAAAFF;
+        border: 0;
+        font-size: 24px;
+        font-family: 'Roboto';
+        text-align: right;
+        padding: 0 10px;
+        color: #FFFFFF;  
+    }
+`;
